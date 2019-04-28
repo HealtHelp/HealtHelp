ce7b7dea9e91d99cb46303fa5fc1463d606cf786
BladimirDelBarrioPizarro <bladimaltego@gmail.com>
2019-04-28 16:53:25 +0200

    fix: Git repository origin

--- src/main/java/com/api/healthelp/HealthelpApplication.java
+++ src/main/java/com/api/healthelp/HealthelpApplication.java
@@ -7,10 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HealthelpApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HealthelpApplication.class, args);
 	}
-
 }
 
