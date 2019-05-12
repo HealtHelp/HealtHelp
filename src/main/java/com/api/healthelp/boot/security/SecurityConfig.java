9064708a97429fe8c9d87862275e6f44ef4bbf65
BladimirDelBarrioPizarro <bladimaltego@gmail.com>
2019-05-12 14:34:44 +0200

    fix: Added implementation in SecurityConfig class

--- src/main/java/com/api/healthelp/boot/security/SecurityConfig.java
+++ src/main/java/com/api/healthelp/boot/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.api.healthelp.boot.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,22 +11,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("healthelp")).authorities("ADMIN");
-        auth.inMemoryAuthentication().withUser("emple1").password(passwordEncoder().encode("emple1")).authorities("USER");
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("healthelp")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("emple1").password(passwordEncoder().encode("emple1")).roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().and().cors().disable()
+        http
+                .csrf().disable()
                 .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .logout()
+                .antMatchers(HttpMethod.GET,"/healthelp/users").hasAnyRole("ADMIN","USER")
+                .antMatchers(HttpMethod.POST,"/healthelp/user").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/healthelp/user").hasAnyRole("ADMIN")
                 .and()
                 .httpBasic();
     }
