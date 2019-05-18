bf2b8b6d0bcd219b0286f4b2686bf8661a4544d9
BladimirDelBarrioPizarro <bladimaltego@gmail.com>
2019-05-19 01:27:19 +0200

    fix: remove server.servlet.context-path

--- src/main/java/com/api/healthelp/HealthelpApplication.java
+++ src/main/java/com/api/healthelp/HealthelpApplication.java
@@ -2,10 +2,11 @@ package com.api.healthelp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HealthelpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HealthelpApplication.class, args);
